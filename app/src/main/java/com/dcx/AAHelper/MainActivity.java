d82c778aaaff4d1addd5498d24ccb5a61bcbec7f
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-08-21 18:08:26 +0800

    添加：主页向其他页面的跳转关系

--- app/src/main/java/com/dcx/AAHelper/MainActivity.java
+++ app/src/main/java/com/dcx/AAHelper/MainActivity.java
@@ -1,8 +1,15 @@
 package com.dcx.AAHelper;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.dcx.AAHelper.AAActivities.DayAAActivity;
+import com.dcx.AAHelper.AAActivities.PersonAAActivity;
+import com.dcx.AAHelper.AAActivities.SimpleAAActivity;
+import com.dcx.AAHelper.Utils.ToastMaker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToastMaker.showShortToast(this, getString(R.string.homepageMessage));
+    }
+
+    public void simpleAA(View view) {
+        Intent intent = new Intent(this, SimpleAAActivity.class);
+        startActivity(intent);
+    }
+
+    public void dayAA(View view) {
+        Intent intent = new Intent(this, DayAAActivity.class);
+        startActivity(intent);
+    }
+
+    public void personAA(View view) {
+        Intent intent = new Intent(this, PersonAAActivity.class);
+        startActivity(intent);
     }
 }
