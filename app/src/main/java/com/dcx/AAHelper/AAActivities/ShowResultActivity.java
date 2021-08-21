34067de4d2b86f38f5046e10f3b5a3699e5ca976
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-08-21 18:00:41 +0800

    添加：展示计算结果页面的活动

---
+++ app/src/main/java/com/dcx/AAHelper/AAActivities/ShowResultActivity.java
@@ -0,0 +1,16 @@
+package com.dcx.AAHelper.AAActivities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dcx.AAHelper.R;
+
+public class ShowResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_result);
+    }
+}
