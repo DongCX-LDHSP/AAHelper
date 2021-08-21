360b757945babe280cd570decee619e68310bc45
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-08-21 17:59:13 +0800

    添加：三种不同的均摊方式页面

---
+++ app/src/main/java/com/dcx/AAHelper/AAActivities/DayAAActivity.java
@@ -0,0 +1,16 @@
+package com.dcx.AAHelper.AAActivities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dcx.AAHelper.R;
+
+public class DayAAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_a_a);
+    }
+}
---
+++ app/src/main/java/com/dcx/AAHelper/AAActivities/PersonAAActivity.java
@@ -0,0 +1,16 @@
+package com.dcx.AAHelper.AAActivities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dcx.AAHelper.R;
+
+public class PersonAAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_a_a);
+    }
+}
