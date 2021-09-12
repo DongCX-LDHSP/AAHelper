83a11046c8c16895c167b0f0df6cfd3a2d6bae90
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-09-12 17:11:35 +0800

    实现：计算两个日期之间相差天数的静态方法

--- app/src/main/java/com/dcx/AAHelper/Utils/CountDays.java
+++ app/src/main/java/com/dcx/AAHelper/Utils/CountDays.java
@@ -1,5 +1,20 @@
 package com.dcx.AAHelper.Utils;
 
+import java.util.Date;
+
 public class CountDays {
+    // 一天的秒数
+    public static final long oneDayMilliseconds = 86400000;
+
+    /**
+     * 计算日期[dateA, dateB]之间的天数，包含dateA和dateB
+     *
+     * @param dateA 一个日期
+     * @param dateB 另一个日期
+     * @return 这两个日期之间相差的天数
+     */
+    public static long daysBetween(Date dateA, Date dateB) {
+        return Math.abs((dateB.getTime() - dateA.getTime()) / oneDayMilliseconds) + 1;
+    }
 
 }
